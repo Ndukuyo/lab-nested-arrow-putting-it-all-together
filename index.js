2d8af8ccd67e99b437b00ecacb75e3ab945fca8c
Ndukuyo <harrisonndukuyo@gmail.com>
2025-11-28 16:32:16 +0300

    Innitial commit

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  const maxAttempts = 3;
 
+  return (passwordAttempt) => {
+
+     if (attemptCount >= maxAttempts) {
+      return "Account locked due to too many failed attempts";
+
+    } 
+
+    attemptCount++;
+
+    if (passwordAttempt === userInfo.password && attemptCount <= maxAttempts) {
+      return "Login successsful ";
+
+    }
+
+    if (attemptCount <= maxAttempts) {
+      return `${attemptCount} attempt, Login failed`;
+    }
+
+  };
+
+}
 
 
 module.exports = {
